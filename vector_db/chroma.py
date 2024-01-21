6f9774a02ad2e56deda1b8fae601befb74efe3ba
Matteo Citterio <matteo.citterio@bip-group.com>
2024-01-21 17:43:51 +0100

    . local Chroma implementation as a vector storage . implementation of the utilities for the vector storages

---
+++ vector_db/chroma.py
@@ -0,0 +1,55 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+
+
+class EnhVectorDatabase:
+
+    def __init__(self, collection_name: str = "embeddings"):
+        self.chroma_client = chromadb.Client()
+        self.custom_encoder = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+        self.collections = {
+            collection_name: self.chroma_client.create_collection(name=collection_name)
+        }
+
+    def create_new_collection(self, collection_name: str):
+        if collection_name in self.collections:
+            raise Exception(f"Already existing collection with name {collection_name}.")
+        self.collections.update(
+            {collection_name: self.chroma_client.create_collection(name=collection_name)}
+        )
+
+    def insert_new_chunks(self, collection_name: str, documents: [], use_default_encoding: bool = True):
+
+        collection = self.collections[collection_name]
+
+        if not use_default_encoding:  # using custom encoding with sBERT
+            embeddings = self.custom_encoder.encode(documents)
+            collection.add(
+                embeddings=embeddings,
+                documents=documents,
+                metadatas=[{"source": "test"} for _ in range(len(documents))],
+                ids=[str(i) for i in range(len(documents))]
+            )
+
+        else:  # using default chroma encoding
+            collection.add(
+                documents=documents,
+                metadatas=[{"source": "test"} for _ in range(len(documents))],
+                ids=[str(i) for i in range(len(documents))]
+            )
+
+    def get_most_similar_documents(self, collection_name: str, query_text: str, n=1, use_default_encoding=True):
+        # obtain "query_embedding" by using sBERT
+        collection = self.collections[collection_name]
+
+        if use_default_encoding:
+            return collection.query(
+                query_texts=[query_text],
+                n_results=n
+            )
+
+        query_embedding = self.custom_encoder.encode([query_text])[0].tolist()
+        return collection.query(
+            query_embeddings=[query_embedding],
+            n_results=n
+        )
